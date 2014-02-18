1d192f3e14afdf8a25e6fef2a722417a76241cd9
fifra <fifra0@gmail.com>
2014-02-18 13:24:30 +0100

    Unity files
    Initialising

---
+++ Pongers/Assets/Scripts/SphereGenerator.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+//a rigid body that links to the sphere prefab
+var onesphere:Rigidbody;
+var twosphere:Rigidbody;
+var threesphere:Rigidbody;
+var foursphere:Rigidbody;
+//the total number of spheres to generate
+var maximumspheres:int;
+
+function generatespheres(){
+	var colornumber:int;
+	colornumber = Random.Range(1,5);
+	Debug.Log(colornumber);
+	var xposition:float;
+	xposition = Random.Range(-2.5f,2.5f);
+	switch(colornumber)
+	{
+		case 1:
+			Instantiate(onesphere,Vector3(xposition,5,0), Quaternion.identity);
+			break;
+		case 2:
+			Instantiate(twosphere,Vector3(xposition,5,0), Quaternion.identity);
+			break;
+		case 3:
+			Instantiate(threesphere,Vector3(xposition,5,0), Quaternion.identity);
+			break;
+		case 4:
+			Instantiate(foursphere,Vector3(xposition,5,0), Quaternion.identity);
+			break;
+	}
+}
+
+function Start () {
+	//run the generate sphere method
+	//first parameter is the startup delay
+	//second paramiter is how often it will run
+	InvokeRepeating("generatespheres",1.0,1.0);
+}
+
+function Update () {
+	
+}
